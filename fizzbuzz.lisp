18b215fd63b2da87e463767735fdd6577a9f71f6
Lukas <77298041+lu-make@users.noreply.github.com>
2021-03-23 14:54:37 +0100

    Add files via upload

---
+++ fizzbuzz.lisp
@@ -0,0 +1,19 @@
+(defparameter *n* 0)
+(defun FizzBuzz ()
+	(if (<= *n* 100)(
+		let(
+		(d3 (zerop (mod *n* 3)))
+		(d5 (zerop (mod *n* 5)))
+		)
+		(cond 
+		((and d3 d5)(princ "FizzBuzz")(terpri))
+		(d3 (princ "Fizz")(terpri))
+		(d5 (princ "Buzz")(terpri))
+		(t (princ *n*)(terpri))
+		)
+		(incf *n*)
+		(FizzBuzz)
+	)
+	)
+)
+(FizzBuzz)
---
+++ gcd.lisp
@@ -0,0 +1,22 @@
+;;;; Euklidischer Algorithmus: Berechnet größten gemeinsamen Teiler von zwei Zahlen
+(defvar *x*)
+(defvar *y*)
+(defun calc ()
+        (princ "Zahl 1: ")
+        (terpri)
+        (setf *x* (parse-integer (read-line)))
+        (princ "Zahl 2: ")
+        (terpri)
+        (setf *y* (parse-integer (read-line)))
+        (princ "Groesster gemeinsamer Teiler: ")
+        (loop 
+                (cond
+				((> *x* *y*)(setf *x* (- *x* *y*)))
+                ((<= *x* *y*)(setf *y* (- *y* *x*)))
+                )
+        (when (= *x* *y*) (write *x*)(quit))
+		)
+        (terpri)      
+)
+(calc)
+
